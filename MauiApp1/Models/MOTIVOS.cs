83486bce5fd264431e91a07dbd5d7046223866ea
gfilhotvb <gustavo.reis.9933@gmail.com>
2023-10-23 22:44:39 -0300

    TESTE

---
+++ MauiApp1/Models/MOTIVOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MauiApp1.Models
+{
+    public class MOTIVOS
+    {
+        public string COD { get; set; }
+        public string DESC { get; set;}
+
+    }
+}
--- MauiApp1/Models/SB1.cs
+++ MauiApp1/Models/SB1.cs
@@ -9,11 +9,27 @@ namespace MauiApp1.Models
 {
     public class SB1
     {
+        [JsonProperty("B1_COD")]
         public string CODPROD { get; set; }
+
+        [JsonProperty("B1_DESC")]
         public string DESCPROD { get; set;}
+
+        [JsonProperty("B1_UM")]
         public string UN { get; set;}
+
+        [JsonProperty("B1_TIPO")]
         public string TIPO { get; set; }
 
+        [JsonProperty("MOT")]
+        public List<MOTIVOSAD> MOT { get; set; }
+    }
+
+
+    public class MOTIVOSAD
+    {
+        public string COD { get; set; }
+        public string DESC { get; set; }
 
     }
 }
